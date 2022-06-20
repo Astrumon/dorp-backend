8d623214a4b5d2600530fae53b20caaac836525c
d.bidnyk <d.bidnyk@pnn.in.ua>
2022-06-20 20:17:41 +0300

    Init commit

---
+++ src/main/kotlin/ua/Application.kt
@@ -0,0 +1,15 @@
+package ua
+
+import io.ktor.server.engine.*
+import io.ktor.server.cio.*
+import kotlinx.serialization.Serializable
+import ua.plugins.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
+
+

9c3c979a7204e8a9bf268599f307417c57ee72a7
Danylo Bidnyk <d.bidnyk@pnn.in.ua>
2022-11-30 19:21:51 +0200

    Finish SessionTest and some refactor it

--- src/test/kotlin/ua/SessionTest.kt
+++ src/test/kotlin/ua/SessionTest.kt
@@ -1,15 +1,17 @@
 package ua
 
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import org.junit.Test
 import ua.features.session.SessionReceiveRemote
+import ua.features.session.SessionResponseRemote
 import ua.features.session.configureSessionRouting
+import java.util.regex.Pattern
 import kotlin.test.assertEquals
 
 class SessionTest {
@@ -26,15 +28,41 @@ class SessionTest {
             }
         }
 
-    @Test
-    fun testCreateSession() = testApplication {
-        val response = sessionClient.post("/sessions/create") {
+    private suspend fun ApplicationTestBuilder.createSession(httpRequestBuilder: HttpRequestBuilder.() -> Unit) =
+        sessionClient.post(SESSION_ENDPOINT) {
             contentType(ContentType.Application.Json)
+            httpRequestBuilder()
+        }
+
+    @Test
+    fun `session must be created`() = testApplication {
+        val response = createSession {
             setBody(SessionReceiveRemote(2))
         }
-        println(response.bodyAsText())
         assertEquals(HttpStatusCode.OK, response.status)
     }
 
+    @Test
+    fun `count of players must be more or equal than 2`() = testApplication {
+        val invalidParam = SessionReceiveRemote(1)
+        val response = createSession {
+            setBody(invalidParam)
+        }
+        assertEquals(HttpStatusCode.BadRequest, response.status)
+    }
 
+    @Test
+    fun `result must be include uppercase symbols or numbers`() = testApplication {
+        val response = createSession {
+            setBody(SessionReceiveRemote(2))
+        }
+        val sessionCode = response.body<SessionResponseRemote>().sessionCode
+        val isValid = Pattern.compile("[A-Z-\\d]{6}").matcher(sessionCode).find()
+
+        assertEquals(isValid, true)
+    }
+
+    companion object {
+        private const val SESSION_ENDPOINT = "/sessions/create"
+    }
 }
